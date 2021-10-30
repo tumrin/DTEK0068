1929bd22eb79d0ea047d96480ff5c432576bbda8
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-10-30 14:58:48 +0300

    Fixed code so it follows BARR-C standard

--- W01E01_PushLED.X/main.c
+++ W01E01_PushLED.X/main.c
@@ -1,6 +1,8 @@
 /*
  * File:   main.c
- * Author: dtek
+ * Author: Tuomas Rinne
+ * Description: W01E01_PushLED exercise. Lights LED when button is pressed and
+ * switches it off when button is released
  *
  * Created on 29 October 2021, 16:57
  */
@@ -9,15 +11,19 @@
 #include <avr/io.h>
 
 int main(void) {
+    // Set F5(LED) as output and F6(Button) as input
     PORTF.DIR |= PIN5_bm;
     PORTF.DIR &= ~PIN6_bm;
 
     while (1)
     {
-        if(PORTF.IN & PIN6_bm){
+        // If input value of F6 is high switch LED on else switch it off
+        if(PORTF.IN & PIN6_bm)
+        {
             PORTF.OUT |= PIN5_bm;
         }
-        else{
+        else
+        {
             PORTF.OUT &= ~PIN5_bm;
         }
     }
