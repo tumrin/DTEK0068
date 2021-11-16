f40657d68ee29f6999afc1d414720423bb8cf88a
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-11-16 14:52:20 +0200

    Photoresistor reacts to light changes

--- W04E01.X/main.c
+++ W04E01.X/main.c
@@ -8,13 +8,32 @@
 #define F_CPU 333333
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
 #define SERVO_PWM_PERIOD        (0x1046) 
 #define SERVO_PWM_DUTY_NEUTRAL  (0x0138)
 #define SERVO_PWM_DUTY_MAX (0x01A0)
 #define SERVO_PWM_DUTY_MIN (0x00D0)
+#define ADC_SAMPLEN 7
+
+//Bitmasks for numbers on display
+#define ZERO PIN5_bm | PIN4_bm | PIN3_bm | PIN2_bm | PIN1_bm | PIN0_bm
+#define ONE PIN2_bm | PIN1_bm
+#define TWO PIN6_bm | PIN4_bm | PIN3_bm | PIN1_bm | PIN0_bm
+#define THREE PIN6_bm| PIN3_bm | PIN2_bm | PIN1_bm | PIN0_bm
+#define FOUR PIN6_bm | PIN5_bm | PIN2_bm | PIN1_bm
+#define FIVE PIN6_bm | PIN5_bm | PIN3_bm | PIN2_bm| PIN0_bm
+#define SIX PIN6_bm | PIN5_bm | PIN4_bm | PIN3_bm | PIN2_bm | PIN0_bm
+#define SEVEN PIN2_bm | PIN1_bm | PIN0_bm
+#define EIGHT PIN6_bm | PIN5_bm | PIN4_bm | PIN3_bm | PIN2_bm | PIN1_bm \
+              | PIN0_bm
+#define NINE PIN6_bm | PIN5_bm | PIN2_bm | PIN1_bm | PIN0_bm
 
 int main(void) 
 { 
+    uint8_t nums[] = {
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+    };
+    PORTC.DIRSET = 0xFF;
     // Route TCA0 PWM waveform to PORTB 
     PORTMUX.TCAROUTEA |= PORTMUX_TCA0_PORTB_gc; 
     // Set 0-WO2 (PB2) as digital output 
@@ -38,13 +57,18 @@ int main(void)
     //    3. Have digital input buffer disabled 
     // Using AN0 (PD0) (default in ADC0.MUXPOS)   
     // Set PD0 as input 
-    PORTD.DIRCLR = PIN0_bm; 
+    PORTE.DIRCLR = PIN0_bm; 
     // No pull-up, no invert, disable input buffer 
-    PORTD.PIN0CTRL = PORT_ISC_INPUT_DISABLE_gc; 
+    PORTE.PIN0CTRL = PORT_ISC_INPUT_DISABLE_gc; 
     // Use Vdd as reference voltage and set prescaler of 16 
     ADC0.CTRLC |= ADC_PRESC_DIV16_gc | ADC_REFSEL_VDDREF_gc; 
     // Enable (power up) ADC (10-bit resolution is default) 
     ADC0.CTRLA |= ADC_ENABLE_bm;
+    ADC0.SAMPCTRL = ADC_SAMPLEN;
+    
+    ADC0.CTRLE |= ADC_INITDLY_DLY16_gc;
+    
+    ADC0.MUXPOS  = ADC_MUXPOS_AIN8_gc;
     
     while (1) 
     {
@@ -59,10 +83,8 @@ int main(void)
          } 
          // RESRDY bit must be cleared before another conversion. 
          // Either by writing '1' over it or reading value from ADC0.RES. 
-         ADC0.INTFLAGS = ADC_RESRDY_bm; 
-         
-        TCA0.SINGLE.CMP2BUF = SERVO_PWM_DUTY_MIN; 
-        _delay_ms(1000);
-        TCA0.SINGLE.CMP2BUF = SERVO_PWM_DUTY_NEUTRAL; 
+         //ADC0.INTFLAGS = ADC_RESRDY_bm;
+         uint16_t res = ADC0.RES;
+         VPORTC.OUT = nums[res/100];
     }  
 }
