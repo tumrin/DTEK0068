fb1488776b04db12f8b300c8df57785c413bf363
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-12-12 04:10:49 +0200

    dummy task works

--- W07E01_LCD.X/FreeRTOS/Source/dummy.c
+++ W07E01_LCD.X/FreeRTOS/Source/dummy.c
@@ -2,9 +2,46 @@
 #include "FreeRTOS.h" 
 #include "clock_config.h" 
 #include "task.h" 
+#include "semphr.h"
+#include "adc.h"
+#include "timers.h"
+
+void led_timer_callback()
+{
+    xSemaphoreTake(mutex_handle, 100);
+    ADC_result_t adc_result = read_adc();
+    xSemaphoreGive(mutex_handle);
+    if(adc_result.ntc > adc_result.pot)
+    {
+        PORTF.OUTSET = PIN5_bm;
+    }
+    else
+    {
+        PORTF.OUTCLR = PIN5_bm;
+    }
+}
 
 void dummy_task(void *param)
 {
+    PORTF.DIRSET = PIN5_bm;
+    TimerHandle_t led_timer = xTimerCreate
+      ( /* Just a text name, not used by the RTOS
+        kernel. */
+        "led",
+        /* The timer period in ticks, must be
+        greater than 0. */
+        100,
+        /* The timers will auto-reload themselves
+        when they expire. */
+        pdTRUE,
+        /* The ID is used to store a count of the
+        number of times the timer has expired, which
+        is initialised to 0. */
+        ( void * ) 5,
+        /* Each timer calls the same callback when
+        it expires. */
+        led_timer_callback);
+    xTimerStart(led_timer, 0);
     vTaskDelay(200);
     for(;;)
     {
