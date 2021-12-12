360bce3c461086cd712fb53c27c7faf463d033fa
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-12-12 03:31:09 +0200

    backlight changing works

--- W07E01_LCD.X/FreeRTOS/Source/backlight.c
+++ W07E01_LCD.X/FreeRTOS/Source/backlight.c
@@ -2,17 +2,44 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "adc.h"
+#include "timers.h"
+
+ADC_result_t adc_result;
+
+void backlight_timer_callback()
+{
+    xSemaphoreTake(mutex_handle, 100);
+    adc_result = read_adc();
+    xSemaphoreGive(mutex_handle);
+
+    uint16_t ratio = adc_result.ldr*10/1023;
+    TCB3.CCMP = 0xFFFF - (0xFFFF/10*ratio);
+}
 
 void backlight_task(void *param)
 {
-    ADC_result_t adc_result;
-    
+    TimerHandle_t backlight_time = xTimerCreate
+      ( /* Just a text name, not used by the RTOS
+        kernel. */
+        "Backlight",
+        /* The timer period in ticks, must be
+        greater than 0. */
+        100,
+        /* The timers will auto-reload themselves
+        when they expire. */
+        pdTRUE,
+        /* The ID is used to store a count of the
+        number of times the timer has expired, which
+        is initialised to 0. */
+        ( void * ) 2,
+        /* Each timer calls the same callback when
+        it expires. */
+        backlight_timer_callback);
+    xTimerStart(backlight_time, 0);
     
     vTaskDelay(200);
     for(;;)
     {
-        adc_result = read_adc();
-        TCB3.CCMP = adc_result.ldr;
     }
     vTaskDelete(NULL);
 }
